d17eeea2023a9be9494d8ad2d6bd27bb25b04b7f
Kurikichi <katasuke1215@gmail.com>
2020-06-30 19:03:57 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,45 @@
 $(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      let html =
+        `<div class="messagebox">
+          <div class="messageinfo">
+            <div class="messageinfo__username">
+              ${message.user_name}
+            </div>
+            <div class="messageinfo__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message">
+            <p class="message__content">
+              ${message.content}
+            </p>
+            <img class="message__image" src="${message.image}">
+          </div>
+        </div>`
+      return html;
+    } else {
+      let html =
+      `<div class="messagebox">
+        <div class="messageinfo">
+          <div class="messageinfo__username">
+            ${message.user_name}
+          </div>
+          <div class="messageinfo__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message">
+          <p class="message__content">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+      return html;
+    };
+  }
+
   $('.new_message').on('submit', function(e){
     e.preventDefault()
     let formData = new FormData(this);
@@ -10,6 +51,9 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
     })
   });
 });
