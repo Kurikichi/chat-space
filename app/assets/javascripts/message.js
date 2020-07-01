e344fa01d9290848bebebeaa468951aff08b902f
Kurikichi <katasuke1215@gmail.com>
2020-07-01 19:03:12 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -77,12 +77,22 @@ $(function(){
       data: {id: last_message_id}
     })
     .done(function(messages) {
-      console.log('success');
+      // 更新するメッセージがなかった場合は.doneの後の処理が動かないようにする
+      if (messages.length !== 0) {
+        //追加するHTMLの入れ物を作る
+        let insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+        $('.MessageField').append(insertHTML);
+      }
     })
     .fail(function() {
       alert('error');
     });
   };
 
-  // setInterval(reloadmessages, 7000);
+  setInterval(reloadmessages, 7000);
 });
